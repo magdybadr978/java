9e5f4787d9ebe8916db6597a31a6c1ce59d0ffc2
magdybadr978 <magdibadr2002@gmail.com>
2024-11-02 13:44:07 +0200

    update in id

--- demo/src/main/java/com/example/demo/models/user/User.java
+++ demo/src/main/java/com/example/demo/models/user/User.java
@@ -3,13 +3,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 
 @Entity
 @Table(name ="users")
 public class User {
-  @jakarta.persistence.Id
+  @Id
   @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Long id;
 

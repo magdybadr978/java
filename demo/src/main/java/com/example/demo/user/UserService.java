c07231e3b98750309dd8f6a88b2a360aab717b5c
magdybadr978 <magdibadr2002@gmail.com>
2024-11-02 13:44:49 +0200

    add user service logic

--- demo/src/main/java/com/example/demo/user/UserService.java
+++ demo/src/main/java/com/example/demo/user/UserService.java
@@ -1,5 +1,29 @@
 package com.example.demo.user;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.user.User;
+import com.example.demo.models.user.UserRepository;
+
+@Service
 public class UserService {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository){
+    this.userRepository = userRepository;
+  }
+
+  public User createUser(User user){
+    return userRepository.create(user);
+  }
+
+  public Optional<User> getUserById(Long id){
+    return userRepository.findById(id);
+  }
+
   
 }

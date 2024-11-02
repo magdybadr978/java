4723ecd84097e934d58cd037b698b1050c015654
magdybadr978 <magdibadr2002@gmail.com>
2024-11-03 00:05:17 +0200

    post service

--- demo/src/main/java/com/example/demo/post/PostService.java
+++ demo/src/main/java/com/example/demo/post/PostService.java
@@ -4,23 +4,33 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.models.post.Post;
 import com.example.demo.models.post.PostRepository;
+import com.example.demo.models.user.UserRepository;
 
 
 @Service
 public class PostService {
   private final PostRepository postRepository;
+  private final UserRepository userRepository;
 
   @Autowired
-  public PostService(PostRepository postRepository){
+  public PostService(PostRepository postRepository , UserRepository userRepository){
     this.postRepository = postRepository;
+    this.userRepository = userRepository;
   }
 
   // create post 
   public Post createPost(Post post){
+    // check if user exist
+    boolean userExist = userRepository.existsById(post.getUser().getId());
+    // faild
+    if(!userExist) throw new ResponseStatusException(HttpStatus.NOT_FOUND ,"user not found");
+    // return response
     return postRepository.create(post);
   }
 

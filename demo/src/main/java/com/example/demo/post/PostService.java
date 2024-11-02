e530d07ede3df71b70c104ba4411f105ef4f5dce
magdybadr978 <magdibadr2002@gmail.com>
2024-11-02 22:49:12 +0200

    add post service

--- demo/src/main/java/com/example/demo/post/PostService.java
+++ demo/src/main/java/com/example/demo/post/PostService.java
@@ -1,5 +1,47 @@
 package com.example.demo.post;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.post.Post;
+import com.example.demo.models.post.PostRepository;
+
+
+@Service
 public class PostService {
-  
+  private final PostRepository postRepository;
+
+  @Autowired
+  public PostService(PostRepository postRepository){
+    this.postRepository = postRepository;
+  }
+
+  // create post 
+  public Post createPost(Post post){
+    return postRepository.create(post);
+  }
+
+  // get specific post
+  public Optional<Post> getPostById(Long id){
+    return postRepository.findById(id);
+  }
+
+  // get all posts
+  public List<Post> getAllPosts(){
+    return postRepository.findAll();
+  }
+
+  // update post
+  public Post updatePost(Post post){
+    return postRepository.update(post);
+  }
+
+  // delete post
+  public void deletePost(Long id){
+    postRepository.deleteById(id);
+  }
+
 }

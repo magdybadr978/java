7a260f939d636d47882c4483ec28e6460f2db343
magdybadr978 <magdibadr2002@gmail.com>
2024-11-02 13:44:35 +0200

    make user repository

--- demo/src/main/java/com/example/demo/models/user/UserRepository.java
+++ demo/src/main/java/com/example/demo/models/user/UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.models.user;
+import java.util.Optional;
+import com.example.demo.models.AbstractRepository;
 
-public class UserRepository {
-  
+
+
+public interface  UserRepository extends AbstractRepository<User , Long>{
+  Optional<User> findByEmail(String email);
+
 }
